6945ea440653bc1266839e5328f2ecf658f55dde
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2022-01-11 18:35:59 +0800

    monaco-editor实现demo展示

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
 /* eslint-disable */
-import { defineComponent, ref } from "vue";
-import HelloWorld from "./components/HelloWorld";
-const image = require("./assets/logo.png");
+import { defineComponent, Ref, ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
+
+function toJson(data: any): string {
+  return JSON.stringify(data, null, 2)
+}
+const schema = {
+  type: 'string',
+}
 export default defineComponent({
   setup() {
-    const nameRef = ref("liuyong");
+    const nameRef = ref('liuyong')
     setInterval(() => {
-      nameRef.value += "1";
-    }, 1000);
+      nameRef.value += '1'
+    }, 1000)
+
+    const classRef = useStyles()
+
+    const schemaRef: Ref<any> = ref(schema)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (err) {}
+      schemaRef.value = schema
+    }
     return () => {
+      const code = toJson(schemaRef.value)
       return (
-        <div id="app">
-          <img src={image} alt="Vue logo"></img>
-          <p>{nameRef.value}</p>
-          <HelloWorld age={15} />
-          gg
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleCodeChange}
+            title="Schema"
+            class={classRef.value.editor}
+          ></MonacoEditor>
         </div>
-      );
-    };
+      )
+    }
   },
-});
+})
